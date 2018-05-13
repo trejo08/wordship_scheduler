b27c6757cc88749e46ddcdc406200e018eb4bcf2
Juan Trejo <trejocola@gmail.com>
2018-05-13 02:27:37 -0600

    upgrading to rails 5.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'dashboard#index'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :dashboard
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228072811) do
+ActiveRecord::Schema.define(version: 2018_02_28_072811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
